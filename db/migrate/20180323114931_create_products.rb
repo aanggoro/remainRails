91b20275fc539f0a8626c6727df96d78a11620a4
aanggoro <1511501288@student.budiluhur.ac.id>
2018-03-23 19:16:50 +0700

    add id: false and t.string :sku, primary_key: true in migration create products

--- db/migrate/20180323114931_create_products.rb
+++ db/migrate/20180323114931_create_products.rb
@@ -1,7 +1,7 @@
 class CreateProducts < ActiveRecord::Migration[5.1]
   def change
-    create_table :products do |t|
-      t.string :sku
+    create_table :products, id: false do |t|
+      t.string :sku, primary_key: true
 
       t.timestamps
     end
